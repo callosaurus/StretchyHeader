48c06cd0407b381900dde656e62f98003eef7934
callosaurus <callumdavies421@gmail.com>
2017-03-14 11:29:38 -0700

    first commit

---
+++ StretchyHeader/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  StretchyHeader
+//
+//  Created by Callum Davies on 2017-03-14.
+//  Copyright © 2017 Callum Davies. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
